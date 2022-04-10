d33a1c057b50a5ed1f73550a409c045094aad306
Denis Danilenko <denis87dan@gmail.com>
2022-04-10 22:17:57 +0300

    Lesson_12

--- junitlessons/src/main/java/com/andersenlab/Triangle.java
+++ junitlessons/src/main/java/com/andersenlab/Triangle.java
@@ -1,7 +1,7 @@
 package com.andersenlab;
 
 public class Triangle {
-    public double getArea(int a, int b, int c) {
+    public static double getArea(int a, int b, int c) {
         double p = (double) (a + b + c) / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
--- junitlessons/src/test/java/com/andersenlab/TriangleTest.java
+++ junitlessons/src/test/java/com/andersenlab/TriangleTest.java
@@ -6,27 +6,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TriangleTest {
-    private Triangle triangle = new Triangle();
     private static Logger logger = LoggerFactory.getLogger(TriangleTest.class);
 
     @Test
     public void test1() {
         logger.info("Start test 1");
-        Assertions.assertEquals(0.0, triangle.getArea(1, 2, 3));
+        Assertions.assertEquals(0.0, Triangle.getArea(1, 2, 3));
         logger.info("Stop test 1");
     }
 
     @Test
     public void test2() {
         logger.info("Start test 2");
-        Assertions.assertEquals(1.7320508075688772, triangle.getArea(2, 2, 2));
+        Assertions.assertEquals(1.7320508075688772, Triangle.getArea(2, 2, 2));
         logger.info("Stop test 2");
     }
 
     @Test
     public void test3() {
         logger.info("Start test 3");
-        Assertions.assertEquals(2.8284271247461903, triangle.getArea(3, 2, 3));
+        Assertions.assertEquals(2.8284271247461903, Triangle.getArea(3, 2, 3));
         logger.info("Stop test 3");
     }
 }
